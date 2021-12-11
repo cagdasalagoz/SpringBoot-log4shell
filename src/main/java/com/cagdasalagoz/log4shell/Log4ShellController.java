62351d42e8aebbed000565b43289580caeba1974
cagdas <cagdasalagoz@gmail.com>
2021-12-11 15:20:57 +0300

    adds requestWithHeader

--- src/main/java/com/cagdasalagoz/log4shell/Log4ShellController.java
+++ src/main/java/com/cagdasalagoz/log4shell/Log4ShellController.java
@@ -2,18 +2,19 @@ package com.cagdasalagoz.log4shell;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 public class Log4ShellController {
     private static final Logger log = LogManager.getLogger(Log4ShellController.class);
 
-
-    @PostMapping("/logthis")
-    public void createAppointment(@RequestBody LogRequest logRequest) {
+    @PostMapping("/logThis")
+    public void logThis(@RequestBody LogRequest logRequest) {
         log.info("Log: {}", logRequest.getLog());
     }
 
+    @GetMapping("/requestWithHeader")
+    public void requestWithHeader(@RequestHeader("justaheader") String apiVersion) {
+        log.info("header value: {}", apiVersion);
+    }
 }
